9d14b4eb24fc31db8f4ee3a27c8297ca7cf11582
Leopold Olsson <bobbzorzen@gmail.com>
2017-11-15 11:53:41 +0100

    added lab5 calculator and updated it to store all relevant data in the list

---
+++ lab5/lab-05-calculator.py
@@ -0,0 +1,56 @@
+def store(list_of_values, memory_value):
+	pass
+
+def load(list_of_values):
+	pass
+    
+def getIntegerInput(inputText, errorMessage="Invalid number"):
+    while True :
+        try:
+            return int(input(inputText))
+        except:
+            print(errorMessage)
+
+def getOperationInput(inputText, errorMessage="Invalid operation"):
+    while True :
+        operation = input(inputText)
+        if(operation in ["add", "sub", "mul", "div", "quit", "undo", "store", "load"]):
+            return operation
+        else:
+            print(errorMessage)
+
+def calculator():
+    memory_value = getIntegerInput("Enter initial  memory value: ")
+    undo_list = [memory_value]
+    operation = ""
+    while operation != "quit":
+        operation = getOperationInput("Enter operation (add/sub/mul/div/quit): ")
+        if operation == "add":
+            secondary_number = getIntegerInput("Enter operand: ")
+            memory_value = memory_value + secondary_number
+            undo_list.append(memory_value)
+        elif operation == "sub":
+            secondary_number = getIntegerInput("Enter operand: ")
+            memory_value = memory_value - secondary_number
+            undo_list.append(memory_value)
+        elif operation == "mul":
+            secondary_number = getIntegerInput("Enter operand: ")
+            memory_value = memory_value * secondary_number
+            undo_list.append(memory_value)
+        elif operation == "div":
+            if secondary_number == 0:
+                print("Zero division error, please try again")
+            else:
+                secondary_number = getIntegerInput("Enter operand: ")
+                memory_value = memory_value / secondary_number
+                undo_list.append(memory_value)
+        elif operation == "undo":
+            if len(undo_list) > 1:
+                undo_list.pop()
+                memory_value = undo_list[-1]
+            else:
+                print("There is nothing to undo")
+        print(memory_value, " is stored in memory.")
+    return memory_value
+
+print("The program finished with " + str(calculator()) + " in memory.")
